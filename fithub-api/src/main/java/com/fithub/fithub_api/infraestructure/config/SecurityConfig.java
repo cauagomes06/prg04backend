a4ecb6f844ec6d578d110629cc6ca564029e1b48
cauagomes06 <cau201812@gmail.com>
2025-11-22 03:42:58 -0300

    fix(security): Libera rota /api/planos/buscar para acesso publico

--- fithub-api/src/main/java/com/fithub/fithub_api/infraestructure/config/SecurityConfig.java
+++ fithub-api/src/main/java/com/fithub/fithub_api/infraestructure/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/usuarios/register").permitAll()
 
+                        .requestMatchers(HttpMethod.GET, "/api/planos/buscar").permitAll()
                         // Recursos Estáticos e Documentação
                         .requestMatchers("/imagens/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
