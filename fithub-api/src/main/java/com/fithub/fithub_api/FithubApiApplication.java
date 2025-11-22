0d46e6db0b0e2864eff620e99431bc4ac2ffa6e8
cauagomes06 <cau201812@gmail.com>
2025-11-22 01:36:09 -0300

    feat(backend): Adiciona pom.xml e arquivos de configuração para deploy no Render

---
+++ fithub-api/src/main/java/com/fithub/fithub_api/FithubApiApplication.java
@@ -0,0 +1,17 @@
+package com.fithub.fithub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class FithubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FithubApiApplication.class, args);
+	}
+
+}
---
+++ fithub-api/src/test/java/com/fithub/fithub_api/FithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fithub.fithub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
